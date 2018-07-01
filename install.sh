1d5a2b22c67f20f4cd3f7810ffeb37f0eba90edd
devonlee111 <devonlee111@gmail.com>
2018-06-30 18:25:52 -1000

    Port and Install Update
    Updated the port in conf file to use port 80. Added -y flag to apt-get command in install script. Added a test video file.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
 apt upgrade
-apt-get install build-essential libpcre3 libpcre3-dev libssl-dev
+apt-get -y install build-essential libpcre3 libpcre3-dev libssl-dev
 tar -xvf rtmp_nginx-1.15.0.tar
 cd nginx-1.15.0
 ./configure --with-http_ssl_module --and-module=../nginx-rtmp-module
