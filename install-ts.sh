a8071b71eca498bf40e01f066a07a38b66b93982
Devon Lee <devonlee111@gmail.com>
2018-07-22 20:57:46 +0000

    Added File Upload
    Updated server to be able to handle uploads using php scripts. Removed files for old rtmp-module based server.

--- install-ts.sh
+++ install-ts.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 apt update
 apt upgrade
-apt-get -y install build-essential libpcre3 libpcre3-dev libssl-dev
-tar -xvf nginx-ts-server.tar.gz
+apt-get -y install build-essential libpcre3 libpcre3-dev libssl-dev php7.2 php7.2-fpm php7.2-mysql
+service php7.2-fpm start
+tar -xvf nginx-server.tar.gz
 cd nginx-1.15.0
 ./configure --add-module=../nginx-ts-module
 make
 make install
 cd ../
-mv nginx.conf /usr/local/nginx/conf/
-mv html/* /usr/local/nginx/html/
+mkdir /usr/local/nginx/scripts
+mv data/nginx.conf /usr/local/nginx/conf/
+mv data/html/* /usr/local/nginx/html/
+mv data/upload.php /usr/local/nginx/scripts/
+rm -rf data/
